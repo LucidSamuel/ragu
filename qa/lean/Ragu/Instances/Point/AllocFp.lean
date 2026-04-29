3f0aca41d8c7f174f4b68c1e80f3e0ed46fb588c
Gregor <gregor.mitscha-baude@gmx.at>
2026-04-29 11:41:31 +0200

    Refactor point allocation to use prover point hints

--- qa/lean/Ragu/Instances/Point/AllocFp.lean
+++ qa/lean/Ragu/Instances/Point/AllocFp.lean
@@ -1,18 +1,19 @@
 import Ragu.Circuits.Point.Alloc
 import Ragu.Instances.Autogen.Point.AllocFp
-import Ragu.Instances.Point.Hints
 import Ragu.Core
 
 namespace Ragu.Instances.Point.AllocFp
 open Ragu.Instances.Autogen.Point.AllocFp
 
 set_option linter.unusedVariables false in
-def deserializeInput (input : Vector (Expression (F p)) 0) : Var unit (F p) := ()
+def deserializeInput (input : Vector (Expression (F p)) 0) :
+    Var (UnconstrainedDep Circuits.Point.Spec.Point) (F p) :=
+  default
 
 def serializeOutput (output : Var Circuits.Point.Spec.Point (F p)) : Vector (Expression (F p)) 2 :=
   #v[ output.x, output.y ]
 
-def formal_instance : Core.Statements.GeneralFormalInstance where
+def formal_instance : Core.Statements.GeneralFormalWithHintInstance where
   p
   exportedOperations
   exportedOutput
@@ -20,15 +21,12 @@ def formal_instance : Core.Statements.GeneralFormalInstance where
   deserializeInput
   serializeOutput
 
-  -- x reads row 0 from "alloc_square_w", y reads row 2.
   reimplementation := Circuits.Point.Alloc.circuit Circuits.Point.Spec.EpAffineParams
-    (fun h => Hints.readSquareElem h 0 0)
-    (fun h => Hints.readSquareElem h 2 0)
 
   same_constraints := by
     intro input
     simp [Core.Statements.FlatOperation.eraseCompute, List.map,
-      Operations.toFlat, circuit_norm, GeneralFormalCircuit.toSubcircuit, GeneralFormalCircuit.toWithHint,
+      Operations.toFlat, circuit_norm,
       GeneralFormalCircuit.WithHint.toSubcircuit, FormalCircuit.toSubcircuit,
       Circuits.Point.Alloc.circuit, Circuits.Point.Alloc.elaborated, Circuits.Point.Alloc.main,
       Circuits.Element.AllocSquare.circuit, Circuits.Element.AllocSquare.elaborated, Circuits.Element.AllocSquare.main,
--- qa/lean/Ragu/Instances/Point/AllocFq.lean
+++ qa/lean/Ragu/Instances/Point/AllocFq.lean
@@ -1,18 +1,19 @@
 import Ragu.Circuits.Point.Alloc
 import Ragu.Instances.Autogen.Point.AllocFq
-import Ragu.Instances.Point.Hints
 import Ragu.Core
 
 namespace Ragu.Instances.Point.AllocFq
 open Ragu.Instances.Autogen.Point.AllocFq
 
 set_option linter.unusedVariables false in
-def deserializeInput (input : Vector (Expression (F p)) 0) : Var unit (F p) := ()
+def deserializeInput (input : Vector (Expression (F p)) 0) :
+    Var (UnconstrainedDep Circuits.Point.Spec.Point) (F p) :=
+  fun _ => default
 
 def serializeOutput (output : Var Circuits.Point.Spec.Point (F p)) : Vector (Expression (F p)) 2 :=
   #v[ output.x, output.y ]
 
-def formal_instance : Core.Statements.GeneralFormalInstance where
+def formal_instance : Core.Statements.GeneralFormalWithHintInstance where
   p
   exportedOperations
   exportedOutput
@@ -20,14 +21,12 @@ def formal_instance : Core.Statements.GeneralFormalInstance where
   deserializeInput
   serializeOutput
 
-  reimplementation := Circuits.Point.Alloc.circuit Circuits.Point.Spec.EqAffineParams
-    (fun h => Hints.readSquareElem h 0 0)
-    (fun h => Hints.readSquareElem h 2 0)
+  reimplementation := @Circuits.Point.Alloc.circuit p _ Circuits.Point.Spec.EqAffineParams
 
   same_constraints := by
     intro input
     simp [Core.Statements.FlatOperation.eraseCompute, List.map,
-      Operations.toFlat, circuit_norm, GeneralFormalCircuit.toSubcircuit, GeneralFormalCircuit.toWithHint,
+      Operations.toFlat, circuit_norm,
       GeneralFormalCircuit.WithHint.toSubcircuit, FormalCircuit.toSubcircuit,
       Circuits.Point.Alloc.circuit, Circuits.Point.Alloc.elaborated, Circuits.Point.Alloc.main,
       Circuits.Element.AllocSquare.circuit, Circuits.Element.AllocSquare.elaborated, Circuits.Element.AllocSquare.main,
